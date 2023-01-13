3c4240746c182fd56dee59e07417bf2ca0ccc7b5
gmanu0077 <66879853+gmanu0077@users.noreply.github.com>
2023-01-13 10:46:53 +0530

    manu

--- src/Navbar/Nav1.js
+++ src/Navbar/Nav1.js
@@ -5,19 +5,19 @@ import Navbar from 'react-bootstrap/Navbar';
 function Nav1() {
   return (
     <>
-     <Navbar bg="light" variant="dark">
+      <Navbar bg="light" variant="dark">
         <Container>
           <Navbar.Brand href="https://www.simplemail.ai/">
-            <img 
+            <img
               alt=""
               src="https://lh3.googleusercontent.com/LKDLU8ZVMsIacy8PJISNy369_lGxOAIjTy-9rxv0YKALGOMoFRsQ78dAftd8dRjNRIIjOuT4gbENGowVXpYvghA6Ag=w128-h128-e365-rj-sc0x00ffffff"
-              width="40"
-              height="40"
+              width="90"
+              height="90"
               className="d-inline-block align-top"
             />{' '}
-           <label style={{color :'black'}}> Simplemail </label>
+            <label className='h1' style={{ color: 'black', margin: "1.5vw" }}> Simplemail </label>
           </Navbar.Brand>
-          <button className='btn btn-danger'>Logout</button>
+          <button className='btn btn-secondary btn-lg'>Logout</button>
         </Container>
       </Navbar>
     </>
--- src/components/Pricingpage.js
+++ src/components/Pricingpage.js
@@ -6,124 +6,124 @@ import { useNavigate } from 'react-router-dom';
 
 
 const PricingPage = () => {
-     const navigate = useNavigate()
-    const Monthly = ()=>{
-        navigate('/monthly')
-    }
-    const Yearly = ()=>{
-        navigate('/yearly')
-    }
-   
+  const navigate = useNavigate()
+  const Monthly = () => {
+    navigate('/monthly')
+  }
+  const Yearly = () => {
+    navigate('/yearly')
+  }
+
   return (
     <>
-    <Nav1/>
-    <div className='div'>
-     <main class="">
-    <div class="container ">
-    <h4 class="mb-4 text-center  "><strong className='price'>Our Pricing</strong></h4>
-      <section class="text-center">
-        
+      <Nav1 />
+      <div className='div'>
+        <main class="">
+          <div class="container ">
+            <h4 class="mb-4 text-center  "><strong className='price'>Our Pricing</strong></h4>
+            <section class="text-center">
 
-        
 
-        <div class="row gx-lg-5">
 
-          
-          <div class="col-lg-3 col-md-6 mb-4">
 
-         
-            <div class="card">
+              <div class="row gx-lg-5">
 
-              <div class="card-header bg-success py-3">
-                <p class="text-uppercase small mb-2"><strong>Free</strong></p>
-                <h5 class="mb-0">Free</h5>
+
+                <div class="col-lg-3 col-md-6 mb-4">
+
+
+                  <div class="card">
+
+                    <div class="card-header bg-success py-3">
+                      <p class="text-uppercase small mb-2 text-white"><strong>DEMO</strong></p>
+                      <h5 class="mb-0 text-white">Try Free for 15 days</h5>
+                    </div>
+
+                    <div class="card-body">
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Feature</li>
+                        <li class="list-group-item">Feature</li>
+                        <li class="list-group-item">Feature</li>
+                      </ul>
+                    </div>
+
+                    <div class="card-footer bg-white py-3">
+                      <button type="button" class="btn btn-success btn-sm">Free Trial</button>
+                    </div>
+
+                  </div>
+
+
+                </div>
+
+                <div class="col-lg-3 col-md-6 mb-4">
+
+
+                  <div class="card border ">
+
+                    <div class="card-header bg-primary py-3">
+                      <p class="text-uppercase small mb-2 text-white"><strong>Monthly</strong></p>
+                      <h5 class="mb-0 text-white">$6.79</h5>
+                    </div>
+
+                    <div class="card-body">
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Feature</li>
+                        <li class="list-group-item">Feature</li>
+                        <li class="list-group-item">Feature</li>
+                        {/* <li class="list-group-item">Feature</li> */}
+                      </ul>
+                    </div>
+
+                    <div class="card-footer bg-white py-3">
+                      <button type="button" class="btn btn-primary btn-sm" onClick={Monthly}>Buy now</button>
+                    </div>
+
+                  </div>
+
+
+                </div>
+
+
+
+                <div class="col-lg-3 col-md-6 mb-4">
+
+
+                  <div class="card ">
+
+                    <div class="card-header bg-warning py-3">
+                      <p class="text-uppercase small mb-2 text-white"><strong>Yearly</strong></p>
+                      <h5 class="mb-0 text-white">$70</h5>
+                    </div>
+
+                    <div class="card-body ">
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Feature</li>
+                        <li class="list-group-item">Feature</li>
+                        <li class="list-group-item">Feature</li>
+                        {/* <li class="list-group-item">Feature</li>
+                  <li class="list-group-item">Feature</li> */}
+                      </ul>
+                    </div>
+
+                    <div class="card-footer bg-white py-3">
+                      <button type="button" class="btn btn-warning btn-sm text-white" onClick={Yearly}>Buy now</button>
+                    </div>
+
+                  </div>
+
+
+                </div>
+
+
               </div>
-
-              <div class="card-body">
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                </ul>
-              </div>
-
-              <div class="card-footer bg-white py-3">
-                <button type="button" class="btn btn-success btn-sm">Get it</button>
-              </div>
-
-            </div>
-          
+            </section>
 
           </div>
-          
-          <div class="col-lg-3 col-md-6 mb-4">
-
-        
-            <div class="card border ">
-
-              <div class="card-header bg-primary py-3">
-                <p class="text-uppercase small mb-2"><strong>Monthly</strong></p>
-                <h5 class="mb-0">$19/month</h5>
-              </div>
-
-              <div class="card-body">
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                </ul>
-              </div>
-
-              <div class="card-footer bg-white py-3">
-                <button type="button" class="btn btn-primary btn-sm" onClick={Monthly}>Buy now</button>
-              </div>
-
-            </div>
-           
-
-          </div>
-       
-
-         
-          <div class="col-lg-3 col-md-6 mb-4">
-
-          
-            <div class="card ">
-
-              <div class="card-header bg-warning py-3">
-                <p class="text-uppercase small mb-2"><strong>Yearly</strong></p>
-                <h5 class="mb-0">$49/month</h5>
-              </div>
-
-              <div class="card-body ">
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                  <li class="list-group-item">Feature</li>
-                </ul>
-              </div>
-
-              <div class="card-footer bg-white py-3">
-                <button type="button" class="btn btn-warning btn-sm" onClick={Yearly}>Buy now</button>
-              </div>
-
-            </div>
-           
-
-          </div>
-    
-         
-        </div>
-      </section>
-    
-    </div>
-  </main>
-  <Footer/>
-  </div>
-  </>
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 };
 

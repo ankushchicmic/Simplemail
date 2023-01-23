32dec3a54d68b3ea1241c603e8cc1a2c71ae5f73
Prabhakar <prabhakar9169@gmail.com>
2023-01-23 09:36:07 +0530

    change

--- src/Navbar/Nav1.jsx
+++ src/Navbar/Nav1.jsx
@@ -28,7 +28,7 @@ function Nav1() {
               src="https://lh3.googleusercontent.com/LKDLU8ZVMsIacy8PJISNy369_lGxOAIjTy-9rxv0YKALGOMoFRsQ78dAftd8dRjNRIIjOuT4gbENGowVXpYvghA6Ag=w128-h128-e365-rj-sc0x00ffffff"
               width="75"
               height="70"
-              classNameName="d-inline-block align-top"
+              className="d-inline-block align-top"
             />
     <div className="nav-title">
      Simplemail
@@ -43,8 +43,8 @@ function Nav1() {
   </div>
   
   <div className="nav-links">
-     <button classNameName='tut ' onClick={tut}> --&gt;back to Tutorial</button>
-      <button classNameName='btn2 '>Logout</button>
+     <button className='tut ' onClick={tut}> --&gt;back to Tutorial</button>
+      <button className='btn2 '>Logout</button>
   </div>
 </div>
     </>
